8471fae22bbcc33d5648f5711284d78b8d135de5
koki-ota <koki.ota@woven-planet.global>
2024-04-07 09:41:50 +0900

    add 01 02 challenge

--- 01_various/02_challenge/main.cpp
+++ 01_various/02_challenge/main.cpp
@@ -1,1 +1,14 @@
-// コードを入力してください
+#include "./templates.hpp"
+#include <iostream>
+
+int main() {
+    auto result1 = sum(1, 2, 3, 4, 5);
+    auto result2 = sum(10, 20);
+    auto result3 = sum(3);
+
+    std::cout << "result1: " << result1 << std::endl;
+    std::cout << "result2: " << result2 << std::endl;
+    std::cout << "result3: " << result3 << std::endl;
+
+    return 0;
+}
--- 01_various/02_challenge/templates.hpp
+++ 01_various/02_challenge/templates.hpp
@@ -1,1 +1,10 @@
-// コードを入力してください
+template <typename T>
+
+T sum(T n) {
+    return n;
+}
+
+template<typename T, typename... Args>
+T sum(T first, Args... args) {
+    return first + sum(args...);
+}
